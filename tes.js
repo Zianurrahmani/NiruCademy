6505a50b3d2201ba299ff034711175d7b6397cf1
Ｉｌｈａｍ <ilham.addina@gmail.com>
2022-09-16 06:30:54 +0700

    tes

---
+++ tes.js
@@ -0,0 +1,21 @@
+// var toonavatar = require('cartoon-avatar')
+
+
+// User.beforeCreate(user => {
+//   user.profileImg = toonavatar.generate_avatar({"gender":user.gender})
+// })
+
+// const gender = 'male'
+// // toonavatar.generate_avatar();
+
+// var url = toonavatar.generate_avatar({"gender":gender})
+// console.log(url);
+
+
+// static hello(){
+//   return `Hello, ${this.name}!`
+// }
+
+// welcomeUser(){
+//   return `Let's begin your journey today!`
+// }
